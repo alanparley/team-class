ad273b06fcf5f1eb874545d65c3f446bbb7ec23a
Alan Parley <alanparley@hotmail.com>
2020-09-14 16:47:36 +0100

    test running

--- src/team.py
+++ src/team.py
@@ -3,3 +3,12 @@ class Team:
         self.name = name
         self.players = players
         self.coach = coach
+
+    def add_player(self, new_player):
+        self.players.append(new_player)
+
+    def has_player(self, search_player):
+        for player in self.players:
+            if player == search_player:
+                return True
+        return False
