53594a6c8820587cb7ee33b7d439d9e7a5e6b7c8
Alan Parley <alanparley@hotmail.com>
2020-09-14 16:56:03 +0100

    added 0 to points

--- src/team.py
+++ src/team.py
@@ -3,6 +3,7 @@ class Team:
         self.name = name
         self.players = players
         self.coach = coach
+        self.points = 0
 
     def add_player(self, new_player):
         self.players.append(new_player)
